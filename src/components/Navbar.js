ec580d068e96d19b132eb6a9395bb6ee2292570d
pasumaibharatam <pasumaibharatam@gmail.com>
2025-12-28 18:07:03 +0530

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,21 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import React, { useState } from "react";
 
 function Navbar() {
+   const [open, setOpen] = useState(false);
   return (
     <nav className="navbar">
       <div className="logo">PBMK</div>
-
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li>About</li>
-        <li><Link to="/admin">Admin</Link></li>
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/download-id">ID Card</Link></li>
+        <div className="hamburger" onClick={() => setOpen(!open)}>
+        ☰
+      </div>
+      <ul className={`nav-links ${open ? "active" : ""}`}>
+        <li><Link to="/" onClick={() => setOpen(false)}>Home</Link></li>
+        <li><Link to="/register" onClick={() => setOpen(false)}>Register</Link></li>
+        <li><Link to="/download-id" onClick={() => setOpen(false)}>Download ID</Link></li>
+        <li><Link to="/admin" onClick={() => setOpen(false)}>Admin</Link></li>
+        
       </ul>
     </nav>
   );
