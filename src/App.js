507101aa2276751b0c6f475db24316ec3380f4e6
pasumaibharatam <pasumaibharatam@gmail.com>
2026-01-14 10:50:00 +0530

    hello

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import {Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
-// import Register from "./pages/Registerold";
+
 import Register from "./pages/Register"
 import DownloadID from "./pages/DownloadID";
-import Admin from "./pages/Admin";
+
 import Footer from "./components/Footer";
-import AdminLogin from "./pages/AdminLogin";
+
 import AdminDashboard from "./pages/AdminDashboard";
 
 
@@ -19,8 +19,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
          <Route path="/download-id" element={<DownloadID />} />
-         {/* <Route path="/admin" element={<Admin />} />
-         <Route path="/admin-login" element={<AdminLogin/>}/> */}
          <Route path="/admin" element={<AdminDashboard/>}/>
       </Routes>
    
